7357e5a080f546f7630851774b456434fdf4890b
erocher88 <erocher88@outlook.com>
2023-08-02 23:29:13 -0600

    datos de alumnos

---
+++ alumnos.sql
@@ -0,0 +1,3 @@
+INSERT INTO alumnos (FirstName, LastName, Age) VALUES ('Eduardo', 'Rocher', 35);
+INSERT INTO alumnos (FirstName, LastName, Age) VALUES ('Omar', 'Gonzalez', 27);
+INSERT INTO alumnos (FirstName, LastName, Age) VALUES ('Shirley', 'Virginia', 27);
